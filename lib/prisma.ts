0f2b0523147e1037e5dd3821b61d201f1f1963c8
Temulenz <amitekik@gmail.com>
2025-12-03 12:43:31 +0800

    s

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,6 +5,7 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
+console.log(process.env.DATABASE_URL);
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
