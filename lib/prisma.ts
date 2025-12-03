5722e65431ba7ff6097b8dc2a861b261641188a8
jasu55 <67340668+jasu55@users.noreply.github.com>
2025-12-03 13:50:44 +0800

    28 jamka performers finish (#29)
    * 11
    
    * 333

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
-export const prisma = global.prisma ?? new PrismaClient({ log: ["query"] });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
